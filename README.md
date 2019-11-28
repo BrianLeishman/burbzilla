d9a1d9f1a414e42705febdf31a7023bd4f208b12
Brian Leishman <b@rian.gg>
2019-11-28 11:37:43 -0500

    first working demo

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # burbzilla
+
+Writing a new dash board/gauge cluster for my 1999 (and possibly my other 1978) Suburban in Golang to monitor all the sensor in my carbed, stroked 383, sbc.
+
+Finna slap.
--- main.go
+++ main.go
@@ -1,93 +1,62 @@
 package main
 
 import (
-	"encoding/binary"
 	"fmt"
-	"log"
 	"time"
 
-	"github.com/mrmorphic/hwio"
+	"github.com/MichaelS11/go-ads"
 )
 
-func readADC(device hwio.I2CDevice, channel int, gain float64, dataRate int) int {
-	if channel < 0 || channel > 3 {
-		panic("channel must be a value within 0-3")
-	}
-
-	// return self._read(channel + 0x04, gain, data_rate, ADS1x15_CONFIG_MODE_SINGLE)
-	mux := channel + 0x04
-
-	config := ADS1x15_CONFIG_OS_SINGLE
-
-	config |= (mux & 0x07) << ADS1x15_CONFIG_MUX_OFFSET
-
-	if _, ok := ADS1x15_CONFIG_GAIN[gain]; !ok {
-		panic("gain must be one of: 2/3, 1, 2, 4, 8, 16")
-	}
-	config |= ADS1x15_CONFIG_GAIN[gain]
-
-	config |= ADS1x15_CONFIG_MODE_SINGLE
-
-	if _, ok := ADS1115_CONFIG_DR[dataRate]; !ok {
-		panic("data rate must be one of: 8, 16, 32, 64, 128, 250, 475, 860")
-	}
-	config |= ADS1115_CONFIG_DR[dataRate]
-
-	config |= ADS1x15_CONFIG_COMP_QUE_DISABLE
-
-	buf := make([]byte, 4)
-	binary.LittleEndian.PutUint32(buf, uint32((config>>8)&0xFF))
-	if err := device.Write(ADS1x15_POINTER_CONFIG, buf[:2]); err != nil {
-		panic(err)
-	}
-	binary.LittleEndian.PutUint32(buf, uint32(config&0xFF))
-	if err := device.Write(ADS1x15_POINTER_CONFIG, buf[:2]); err != nil {
-		panic(err)
-	}
-
-	time.Sleep(time.Millisecond)
-
-	result0, err := device.Read(ADS1x15_POINTER_CONVERSION, 2)
-	if err != nil {
-		panic(err)
-	}
-	result1, err := device.Read(ADS1x15_POINTER_CONVERSION, 2)
-	if err != nil {
-		panic(err)
-	}
-
-	return conversionValue(int(binary.LittleEndian.Uint16(result0)), int(binary.LittleEndian.Uint16(result1)))
-}
-
-func conversionValue(low, high int) int {
-	value := ((high & 0xFF) << 8) | (low & 0xFF)
-	if value&0x8000 != 0 {
-		value -= 1 << 16
-	}
-	return value
-}
-
 func main() {
-	m, e := hwio.GetModule("i2c")
-	if e != nil {
-		fmt.Printf("could not get i2c module: %s\n", e)
-		return
+	err := ads.HostInit()
+	if err != nil {
+		fmt.Println(err)
 	}
-	i2c := m.(hwio.I2CModule)
-
-	// Uncomment on Raspberry pi, which doesn't automatically enable i2c bus. BeagleBone does,
-	// as the default device tree enables it.
-
-	i2c.Enable()
-	defer i2c.Disable()
-
-	device := i2c.GetDevice(0x4a)
 
 	for {
 		for i := 0; i < 4; i++ {
-			log.Println(i, float32(readADC(device, i, 2/3, 128))/float32(1<<15)*6.144)
-		}
+			// create new ads with wanted busName and address.
+			var ads1 *ads.ADS
+			ads1, err = ads.NewADS("I2C1", 0x4a, "")
+			if err != nil {
+				fmt.Println(err)
+			}
 
-		time.Sleep(time.Second)
+			// example changing config gain (2/3 is default, so only an example)
+			ads1.SetConfigGain(ads.ConfigGain2_3)
+			ads1.SetConfigDataRate(ads.ConfigDataRate860)
+
+			var mux ads.ConfigInputMultiplexer
+			switch i {
+			case 0:
+				mux = ads.ConfigInputMultiplexerSingle0
+			case 1:
+				mux = ads.ConfigInputMultiplexerSingle1
+			case 2:
+				mux = ads.ConfigInputMultiplexerSingle2
+			case 3:
+				mux = ads.ConfigInputMultiplexerSingle3
+			}
+			ads1.SetConfigInputMultiplexer(mux)
+
+			var result uint16
+			result, err = ads1.Read()
+			if err != nil {
+				ads1.Close()
+				fmt.Println(err)
+			}
+
+			// close ads bus
+			err = ads1.Close()
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			// print results
+			fmt.Printf("% 12f", float64(result)/((1<<15)-1)*6.144)
+		}
+		time.Sleep(time.Millisecond)
+
+		fmt.Println()
 	}
 }
